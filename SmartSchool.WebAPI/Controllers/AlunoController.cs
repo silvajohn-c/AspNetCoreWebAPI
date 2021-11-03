53fe7b24fdeb0ba410cea8f42e9c1643fa7c2f0e
Eitydash <silvajohn.costa@gmail.com>
2021-11-03 17:23:55 -0300

    Add: Controllers [ Aluno, Professor ]

---
+++ SmartSchool.WebAPI/Controllers/AlunoController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace SmartSchool.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AlunoController : ControllerBase
+    {
+        public AlunoController() { }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Alunos");
+        }
+    }
+}
