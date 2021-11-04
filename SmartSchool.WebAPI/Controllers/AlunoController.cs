8aa770dc108b6a3b823300fa2b1876d1aad07465
Eitydash <silvajohn.costa@gmail.com>
2021-11-04 15:32:49 -0300

    Add: CRUD [ AlunoController ]

--- SmartSchool.WebAPI/Controllers/AlunoController.cs
+++ SmartSchool.WebAPI/Controllers/AlunoController.cs
@@ -1,4 +1,7 @@
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using SmartSchool.WebAPI.Models;
 
 namespace SmartSchool.WebAPI.Controllers
 {
@@ -8,10 +11,66 @@ namespace SmartSchool.WebAPI.Controllers
     {
         public AlunoController() { }
 
+        public List<Aluno> Alunos = new List<Aluno> {
+            new Aluno(
+                1,
+                "Jhully",
+                "Hermes",
+                "27996584587"
+            ),
+            new Aluno(
+                2,
+                "Jenny",
+                "Mejia",
+                "27996584587"
+            ),
+            new Aluno(
+                3,
+                "John",
+                "Silva",
+                "27996584587"
+            ),
+
+        };
+
         [HttpGet]
         public IActionResult Get()
         {
-            return Ok("Alunos");
+
+            return Ok(Alunos);
+        }
+
+        [HttpGet("{id}")]
+
+        public IActionResult GetById(int id)
+        {
+            var aluno = Alunos.FirstOrDefault(a => a.Id == id);
+            if (aluno == null) return BadRequest("Aluno não encontrado");
+            return Ok(aluno);
+        }
+        [HttpPost]
+        public IActionResult Post(Aluno aluno)
+        {
+
+            return Ok(aluno);
+        }
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, Aluno aluno)
+        {
+
+            return Ok(aluno);
+        }
+        [HttpPatch("{id}")]
+        public IActionResult Patch(int id, Aluno aluno)
+        {
+
+            return Ok(aluno);
+        }
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+
+            return Ok();
         }
     }
 }
